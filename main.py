cf369d2df399bef5aea961e9b74cb523c8f24261
Jinho Kim <jinhokim.kor@gmail.com>
2022-10-16 19:37:19 +0200

    .

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import pathlib
 from cabon_sound import cabon_sound
 import json
 from utils import build_args
 
 if __name__ == "__main__":
-    config = json.load(open("config.json"))
+    config_path = pathlib.Path(input("Input config path: "))
+    config = json.load(open(config_path / "config.json"))
     args = build_args(config)
 
     cabon_sounder = cabon_sound(args)
